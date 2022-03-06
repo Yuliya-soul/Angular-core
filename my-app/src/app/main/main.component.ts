3e8599911a262804fc0371613c5b774d70b0658d
Your Name <darki@tut.by>
2022-03-06 21:37:27 +0300

    add:viewChild samples

--- my-app/src/app/main/main.component.ts
+++ my-app/src/app/main/main.component.ts
@@ -1,17 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {  AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { VideoListComponent } from '../video-list/video-list.component';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit,AfterViewInit {
+  @ViewChild(VideoListComponent, { static: false })
+  videoList!: VideoListComponent;
 
+  @ViewChild('pRef', { static: false })
+  pRef!: ElementRef;
+
+ 
   ngOnInit(): void {
+  }
+  ngAfterViewInit() { 
+    console.log('Here we get child props ', this.videoList.section); 
+    console.log(this.pRef.nativeElement.innerHTML); 
+   this.pRef.nativeElement.innerHTML = "Courses list"; 
   }
   sectionName='Courses';
   clicks:number = 0;
-  onChanged(increased:any){
+  onChangeParent(increased:any){
       increased==true?this.clicks++:this.clicks--;
   }
 }
