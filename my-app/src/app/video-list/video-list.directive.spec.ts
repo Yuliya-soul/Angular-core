614e0d764946da3ce67116ff1626e6d1bb87157a
Your Name <darki@tut.by>
2022-03-07 14:02:39 +0300

    update:design

--- my-app/src/app/video-list/video-list.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { VideoListDirective } from './video-list.directive';
-
-describe('VideoListDirective', () => {
-  it('should create an instance', () => {
-    const directive = new VideoListDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- my-app/src/app/video-list/video-list.directive.ts
+++ my-app/src/app/video-list/video-list.directive.ts
@@ -1,14 +1,32 @@
-import { Directive,ElementRef } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+} from '@angular/core';
 
 @Directive({
-  selector: '[appVideoListBold]'
+  selector: '[appVideoListBold]',
 })
 export class VideoListDirective {
+  constructor(private elemRef: ElementRef) {
+    this.elemRef.nativeElement.style.fontWeight = 'bold';
+  }
+  private fontWeight = 'normal';
 
-  constructor(private elemRef:ElementRef) {   
-    this.elemRef.nativeElement.style.fontWeight ='bold';
-
+  @HostBinding('style.fontWeight') get getFontWeight() {
+    return this.fontWeight;
   }
 
+  @HostBinding('style.cursor') get getCursor() {
+    return 'pointer';
+  }
 
+  @HostListener('mouseenter') onMouseEnter() {
+    this.fontWeight = 'bold';
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.fontWeight = 'normal';
+  }
 }
